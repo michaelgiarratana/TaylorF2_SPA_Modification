5266f2b7501fe806322b32e6657563a230ec6cae
michaelgiarratana <giarratanamd@gmail.com>
2025-03-07 14:25:37 -0500

    Added main code for running from frame files

--- generate_frames.sh
+++ generate_frames.sh
@@ -4,8 +4,8 @@ python3 generate_signal.py \
 	--config parameters.txt \
 	--psd_file lalsim.SimNoisePSDaLIGOZeroDetHighPowerGWINC \
 	--outputfile outputfile.gwf \
-	--channel TestMOD \
-	--det H1 \
+	--channel Test400-L1 \
+	--det L1 \
 	--eventT 1197008880 \
 	--ra 0 \
 	--dec 0 \
