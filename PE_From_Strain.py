c248bef29efd657036e809e2d97f18ce01e1e1f4
michaelgiarratana <giarratanamd@gmail.com>
2025-09-22 08:02:01 -0400

    Updating PE_From_Strain.py

--- PE_From_Strain.py
+++ PE_From_Strain.py
@@ -1,4 +1,4 @@
-# PE from standard method of injecting the parameters
+
 import numpy as np
 import bilby
 import lalsimulation as lalsim
@@ -13,7 +13,6 @@ from lalframe import FrWriteREAL8TimeSeries
 import pickle
 import argparse
 
-# New PE code where bilby injects the modified hp, hc data from a pickle file.
 
 # Argument parsing setup
 parser = argparse.ArgumentParser(description="Process parameter configuration file.")
@@ -32,25 +31,21 @@ parser.add_argument("--out_dir", type=str, required=True, help="Directory to hol
 parser.add_argument("--run_label", type=str, required=True, help="Label for output files.")
 parser.add_argument("--source_type", type=str, required=True, help="Type of binary (BBH, BNS, BHNS)")
 parser.add_argument("--injection_type", type=str, required=True, help="Method of signal injection (bilby or pickle)")
+parser.add_argument("--detector", type=str, required=True, help="GW detector used to recover system.")
 parser.add_argument("--strain_path", type=str, required=True, help="Path to strain pickle file.")
+parser.add_argument("--duration", type=float, required=True, help="Duration of data segment that we're injecting into.")
+parser.add_argument("--begin", type=float, required=True, help="Signal start time.")
 
 args = parser.parse_args()  # Parse the arguments
 
 
-# Set the duration and sampling frequency of the data segment that we're
-# going to inject the signal into
+# Set the duration and sampling frequency of the data segment we are injecting into
+duration = args.duration
 
-if args.source_type == 'BBH':
-    duration = 32
-elif args.source_type == "BNS":
-    duration = 128*2
-
-#duration = 32.0 # for BBH
-#duration = 128.0 # for BNS
 sampling_frequency = 4096.0
 
 # Specify the output directory and the name of the simulation.
-outdir = "/home/giarratana/TaylorF2_SPA_Modification/results/"
+outdir = "/home/giarratana/TaylorF2_SPA_Modification/july_results/"
 label = args.run_label
 #bilby.core.utils.setup_logger(outdir=outdir, label=label)
 
@@ -67,7 +62,7 @@ mc = ((m1*m2)**(3/5))/((m1+m2)**(1/5))
 a1 = args.s1
 a2 = args.s2
 dL = args.distance
-chi_eff = (m1*a1 + m2*a2)/(M)
+chi_effect = (m1*a1 + m2*a2)/(M)
 
 # Defining injection params dict
 injection_parameters = dict(
@@ -93,7 +88,7 @@ injection_parameters = dict(
 )
 
 # Slight modification to the injections params for BNS
-if args.source_type == 'BNS':
+if args.source_type in ["BNS", "NSBH"]:
     # Replace a_1, a_2 with chi_1, chi_2
     injection_parameters['chi_1'] = injection_parameters.pop('a_1')
     injection_parameters['chi_2'] = injection_parameters.pop('a_2')
@@ -116,7 +111,7 @@ source_model_map = {
         "BBH": bilby.gw.source.lal_binary_black_hole,
         "BNS": bilby.gw.source.lal_binary_neutron_star,
         #"BNS": bilby.gw.source.binary_neutron_star_roq,
-        "BHNS": bilby.gw.source.lal_binary_neutron_star
+        "NSBH": bilby.gw.source.lal_binary_neutron_star
 }
 
 # Set the source model based on gw source from args.source_type
@@ -127,25 +122,33 @@ waveform_generator = bilby.gw.WaveformGenerator(
     duration=duration,
     sampling_frequency=sampling_frequency,
     frequency_domain_source_model=frequency_domain_source_model,
-    #parameter_conversion=bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters,
+    parameter_conversion=bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters,
     waveform_arguments=waveform_arguments,
 )
 
-if args.source_type == "BBH":
-    begin = 4, # for BBH
-elif args.source_type == "BNS":
-    begin = 100, # for BNS
 
+if args.detector == "LIGO":
+    # Set up interferometers and the psd/asd from a corresponding txt file
+    ifos = bilby.gw.detector.InterferometerList(["H1", "L1"])
+    ifos[0].power_spectral_density = bilby.gw.detector.PowerSpectralDensity(asd_file = "/home/giarratana/TaylorF2_SPA_Modification/aLIGO_ZERO_DET_high_P_asd.txt")
+    ifos[1].power_spectral_density = bilby.gw.detector.PowerSpectralDensity(asd_file = "/home/giarratana/TaylorF2_SPA_Modification/aLIGO_ZERO_DET_high_P_asd.txt")
+    #ifos[0].power_spectral_density = bilby.gw.detector.PowerSpectralDensity(asd_file = "/home/giarratana/TaylorF2_SPA_Modification/CE_wb_asd.txt")
+    #ifos[1].power_spectral_density = bilby.gw.detector.PowerSpectralDensity(asd_file = "/home/giarratana/TaylorF2_SPA_Modification/CE_wb_asd.txt")
 
-# Set up interferometers and the psd/asd from a corresponding txt file
-ifos = bilby.gw.detector.InterferometerList(["H1", "L1"])
-ifos[0].power_spectral_density = bilby.gw.detector.PowerSpectralDensity(asd_file = "/home/giarratana/TaylorF2_SPA_Modification/aLIGO_ZERO_DET_high_P_asd.txt")
-ifos[1].power_spectral_density = bilby.gw.detector.PowerSpectralDensity(asd_file = "/home/giarratana/TaylorF2_SPA_Modification/aLIGO_ZERO_DET_high_P_asd.txt")
-ifos.set_strain_data_from_power_spectral_densities(
-    sampling_frequency=sampling_frequency,
-    duration=duration,
-    start_time=injection_parameters["geocent_time"] - 4, 
-)
+    ifos.set_strain_data_from_power_spectral_densities(
+            sampling_frequency=sampling_frequency,
+            duration=duration,
+            start_time=injection_parameters["geocent_time"] - args.begin, 
+            )
+elif args.detector == "CE":
+    ifos = bilby.gw.detector.InterferometerList(["CE"])
+    ifos[0].power_spectral_density = bilby.gw.detector.PowerSpectralDensity(psd_file = "/home/giarratana/TaylorF2_SPA_Modification/CE_wb_psd.txt")
+    ifos.set_strain_data_from_power_spectral_densities(
+            sampling_frequency=sampling_frequency,
+            duration=duration,
+            start_time=injection_parameters["geocent_time"] - args.begin,
+            )
+else: raise ValueError(f"Unknown detectorr type: {detector}")
 
 # Define the signal injection method
 injection = args.injection_type
@@ -190,72 +193,113 @@ priors = None  # Initialize priors
 
 if source == 'BBH':
     priors = bilby.gw.prior.BBHPriorDict(injection_parameters.copy())
-    priors['geocent_time'] = bilby.core.prior.Uniform(name='geocent_time', minimum=injection_parameters['geocent_time'] - 0.1, maximum=injection_parameters['geocent_time'] + 0.1)
-    priors['phase'] = bilby.core.prior.Uniform(name='phase', minimum=0, maximum=np.pi)
+    #priors['geocent_time'] = bilby.core.prior.Uniform(name='geocent_time', minimum=injection_parameters['geocent_time'] - 0.1, maximum=injection_parameters['geocent_time'] + 0.1)
+    priors['phase'] = bilby.core.prior.Uniform(name='phase', minimum=0, maximum=2*np.pi)
     
-    #priors['mass_1'] = bilby.core.prior.Uniform(name='mass_1', minimum=20, maximum=45)
-    #priors['mass_2'] = bilby.core.prior.Uniform(name='mass_2', minimum=20, maximum=45)
+    #priors.pop('chirp_mass')
+    #priors.pop('mass_ratio')
+    #priors.pop('total_mass')
+    #priors.pop('symmetric_mass_ratio')
+    #priors['mass_1'] = bilby.core.prior.Uniform(name='mass_1', minimum=15, maximum=45)
+    #priors['mass_2'] = bilby.core.prior.Uniform(name='mass_2', minimum=15, maximum=45)
     
-    priors.pop("mass_1")
-    priors.pop("mass_2")
-    priors.pop("symmetric_mass_ratio")
-    priors.pop("total_mass")
-    priors['chirp_mass'] = bilby.core.prior.Uniform(name='chirp_mass', minimum=mc*0.95, maximum=mc*1.05)
-    priors['mass_ratio'] = bilby.core.prior.Uniform(name='mass_ratio', minimum=0.1, maximum=1)
+    #priors.pop("mass_1")
+    #priors.pop("mass_2")
+    #priors.pop("symmetric_mass_ratio")
+    #priors.pop("total_mass")
+    #priors['chirp_mass'] = bilby.core.prior.Uniform(name='chirp_mass', minimum=mc*0.95, maximum=mc*1.05)
+    #priors['mass_ratio'] = bilby.core.prior.Uniform(name='mass_ratio', minimum=0.01, maximum=1)
     
     priors.pop('luminosity_distance')
-    priors['luminosity_distance'] = bilby.core.prior.PowerLaw(alpha=2, name='luminosity_distance',minimum=100, maximum=500, unit='Mpc', latex_label='$d_L$')
+    priors['luminosity_distance'] = bilby.core.prior.PowerLaw(alpha=2, name='luminosity_distance',minimum=dL*0.25, maximum=dL*1.75, unit='Mpc', latex_label='$d_L$')
+    
     #priors.pop("mass_1")
     #priors.pop("mass_2")
     #priors.pop("mass_ratio")
     #priors.pop("chirp_mass")
-    #priors['total_mass'] = bilby.core.prior.Uniform(name='total_mass', minimum=8, maximum=20)
-    #priors['symmetric_mass_ratio'] = bilby.core.prior.Uniform(name='symmetric_mass_ratio', minimum=0.1, maximum=0.7)
-    #priors['a_1'] = bilby.core.prior.Uniform(0, 0.9, 'a_1')
-    #priors['a_2'] = bilby.core.prior.Uniform(0, 0.9, 'a_2')
+    #priors['total_mass'] = bilby.core.prior.Uniform(name='total_mass', minimum=0.95*M, maximum=1.05*M)
+    #priors['symmetric_mass_ratio'] = bilby.core.prior.Uniform(name='symmetric_mass_ratio', minimum=0.1, maximum=0.25)
+    
+    priors.pop('chirp_mass')
+    priors.pop('mass_ratio')
+    priors.pop('total_mass')
+    priors.pop('symmetric_mass_ratio')
+    priors['mass_1'] = bilby.core.prior.Uniform(name='mass_1', minimum=m1*0.75, maximum=m1*1.25)
+    priors['mass_2'] = bilby.core.prior.Uniform(name='mass_2', minimum=m2*0.75, maximum=m2*1.25)
+
+    priors['a_1'] = bilby.core.prior.Uniform(0.0, 1, 'a_1')
+    priors['a_2'] = bilby.core.prior.Uniform(0.0, 1, 'a_2')
+
+    #priors['chi_eff'] = bilby.core.prior.Uniform(0, 0.9, 'chi_eff')
 
 elif source == "BNS":
     # Neutron Star Binary prior setup
     # Set up all priors to be equal to a delta function at their values designated in the injection parameters
     priors = bilby.gw.prior.BNSPriorDict(injection_parameters.copy())
-    priors['geocent_time'] = bilby.core.prior.Uniform(name='geocent_time', minimum=injection_parameters['geocent_time'] - 0.1, maximum=injection_parameters['geocent_time'] + 0.1)
-    priors['phase'] = bilby.core.prior.Uniform(name='phase', minimum=0, maximum=np.pi)
+    #priors['geocent_time'] = bilby.core.prior.Uniform(name='geocent_time', minimum=injection_parameters['geocent_time'] - 0.1, maximum=injection_parameters['geocent_time'] + 0.1)
+    priors['phase'] = bilby.core.prior.Uniform(name='phase', minimum=0, maximum=2*np.pi)
     
     #priors['mass_1'] = bilby.core.prior.Uniform(name='mass_1', minimum=20, maximum=45)
     #priors['mass_2'] = bilby.core.prior.Uniform(name='mass_2', minimum=20, maximum=45)
     
     #priors.pop('luminosity_distance')
-    #priors['luminosity_distance'] = bilby.core.prior.PowerLaw(alpha=2, name='luminosity_distance',minimum=20, maximum=100, unit='Mpc', latex_label='$d_L$')
+    priors['luminosity_distance'] = bilby.core.prior.PowerLaw(alpha=2, name='luminosity_distance',minimum=dL*0.25, maximum=dL*1.75, unit='Mpc', latex_label='$d_L$')
 
     priors.pop("mass_1")
     priors.pop("mass_2")
     priors.pop("symmetric_mass_ratio")
     priors.pop("total_mass")
-    priors['chirp_mass'] = bilby.core.prior.Uniform(name='chirp_mass', minimum=1.19, maximum=1.39)
-    priors['mass_ratio'] = bilby.core.prior.Uniform(name='mass_ratio', minimum=0.3, maximum=1)
-    priors['chi_1'] = bilby.core.prior.Uniform(-0.05, 0.05, 'chi_1')
-    priors['chi_2'] = bilby.core.prior.Uniform(-0.05, 0.05, 'chi_2')
+    priors['chirp_mass'] = bilby.core.prior.Uniform(name='chirp_mass', minimum=mc*0.95, maximum=mc*1.05)
+    priors['mass_ratio'] = bilby.core.prior.Uniform(name='mass_ratio', minimum=0.5, maximum=1)
+    
+    #priors.pop('chirp_mass')
+    #priors.pop('mass_ratio')
+    #priors.pop('total_mass')
+    #priors.pop('symmetric_mass_ratio')
+    #priors['mass_1'] = bilby.core.prior.Uniform(name='mass_1', minimum=m1*0.5, maximum=m1*1.5)
+    #priors['mass_2'] = bilby.core.prior.Uniform(name='mass_2', minimum=m2*0.5, maximum=m2*1.5)
+    
+    priors['chi_1'] = bilby.core.prior.Uniform(0, 0.1, 'chi_1')
+    priors['chi_2'] = bilby.core.prior.Uniform(0, 0.1, 'chi_2')
 
-elif source == "BHNS":
+elif source == "NSBH":
     # Black Hole/Neutron Star binary prior setup (currently same as BNS)
     # Set up all priors to be equal to a delta function at their values designated in the injection parameters
     priors = bilby.gw.prior.BNSPriorDict(injection_parameters.copy())
+    #priors['geocent_time'] = bilby.core.prior.Uniform(name='geocent_time', minimum=injection_parameters['geocent_time'] - 0.1, maximum=injection_parameters['geocent_time'] + 0.1)
+    priors['phase'] = bilby.core.prior.Uniform(name='phase', minimum=0, maximum=2*np.pi)
+    
+    #priors.pop('chirp_mass')
+    #priors.pop('mass_ratio')
+    #priors.pop('total_mass')
+    #priors.pop('symmetric_mass_ratio')
+    #priors['mass_1'] = bilby.core.prior.Uniform(name='mass_1', minimum=5, maximum=20)
+    #priors['mass_2'] = bilby.core.prior.Uniform(name='mass_2', minimum=1, maximum=3)
+    
+    #priors.pop("mass_1")
+    #priors.pop("mass_2")
+    
+    priors.pop('luminosity_distance')
+    priors['luminosity_distance'] = bilby.core.prior.PowerLaw(alpha=2, name='luminosity_distance',minimum=dL*0.25, maximum=dL*1.75, unit='Mpc', latex_label='$d_L$')
 
-    # Replace fixed component masses with chirp mass and eta
     priors.pop("mass_1")
     priors.pop("mass_2")
+    priors.pop("symmetric_mass_ratio")
+    priors.pop("total_mass")
+    priors['chirp_mass'] = bilby.core.prior.Uniform(name='chirp_mass', minimum=mc*0.95, maximum=mc*1.05)
+    priors['mass_ratio'] = bilby.core.prior.Uniform(name='mass_ratio', minimum=0.01, maximum=0.5)
     
-    priors["chirp_mass"] = bilby.core.prior.Uniform(mc * 0.95, mc * 1.05, "chirp_mass")
-    priors["mass_ratio"] = bilby.core.prior.Uniform(0.125, 1, "mass_ratio")
-    priors['chi_1'] = bilby.core.prior.Uniform(-0.05, 0.05, 'chi_1')
-    priors['chi_2'] = bilby.core.prior.Uniform(-0.05, 0.05, 'chi_2')
-
+    priors['chi_1'] = bilby.core.prior.Uniform(-1, 1, 'chi_1')
+    priors['chi_2'] = bilby.core.prior.Uniform(-0.1, 0.1, 'chi_2')
+    
+    #priors['chi_eff'] = bilby.core.prior.Uniform(0, 0.9, 'chi_eff')
+    #priors['chi_p'] = bilby.core.prior.Uniform(0, 0.9, 'chi_p')
 else:
     raise ValueError(f"Unknown source type '{args.source_type}'. Please choose from 'BBH', 'BNS', or 'BHNS'.")
 
 
 # Perform a check that the prior does not extend to a parameter space longer than the data
-priors.validate_prior(duration, args.f_min)
+#priors.validate_prior(duration, args.f_min)
 
 
 # Compute the appropriate likelihoods based on the source type
@@ -265,9 +309,9 @@ if args.source_type.upper() in ["BNS", "BBH", "NSBH"]:
     likelihood = bilby.gw.likelihood.GravitationalWaveTransient(
         interferometers=ifos,
         waveform_generator=waveform_generator,
-        priors=priors, #For the likelihood marginalization
+        priors=priors,
         distance_marginalization=True,
-       # time_marginalization=True
+        #time_marginalization=True
     )
 elif args.source_type.upper() in []:#"BNS", "NSBH"]: # Currently not in use - returned poor recovery for unknown reason
     # ROQ params for BNS likelihood
@@ -326,25 +370,25 @@ def generate_all_bns_parameters_custom(sample, likelihood = None, priors = None,
 conversion_function_map = {
         "BBH": generate_all_bbh_parameters_custom,
         "BNS": generate_all_bns_parameters_custom,
-        "BHNS": generate_all_bns_parameters_custom,
+        "NSBH": generate_all_bns_parameters_custom,
         }
 
 # Select the appropriate conversion function based on the source
-conversion_function = conversion_function_map.get(args.source_type.upper())
+#conversion_function = conversion_function_map.get(args.source_type.upper())
 
 result = bilby.core.sampler.run_sampler(
     likelihood=likelihood,
-    priors=priors,
-    sampler="dynesty",
-    npoints=3000,
+    priors = priors,
+    sampler = "dynesty",
+    npoints = 2000,
     walks = 100,
-    maxmcmc = 5000,
-    npool = 60,
-    nact=10,
+    maxmcmc = 500,
+    npool = 64,
+    nact = 10,
     injection_parameters=injection_parameters,
     outdir=outdir,
     label=label,
-    #conversion_function=conversion_function,
+    #conversion_function=conversion_function
     sample="unif",
     bootstrap=0,
     resume=True,
